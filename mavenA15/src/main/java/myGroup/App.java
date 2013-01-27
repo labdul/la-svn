e3a23fe9551be25558707fd63750d47d99a0897f
abdulsalam12000 <abdulsalam12000@gmail.com>
2013-01-27 21:16:29 +0000

    new google that works

--- mavenA15/src/main/java/myGroup/App.java
+++ mavenA15/src/main/java/myGroup/App.java
@@ -1,5 +1,88 @@
 package myGroup;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+public class App {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		
+		String eXPath, vURL, vText, vgetText, vgetAttribute, vAttribute; //vBrowser
+		
+		vAttribute = "value";
+		long lTime = 2000;
+			
+		// TODO Auto-generated method stub
+		//System.out.println("Webdriver1");
+		//WebDriver myD = new InternetExplorerDriver();
+		//WebDriver myD = new FirefoxDriver();
+		//WebDriver myD = new HtmlUnitDriver();
+		//WebDriver myD = new IphoneDriver();
+		//WebDriver myD = new AndroidDriver();
+		WebDriver myD = new FirefoxDriver();
+		//vBrowser = "IE";
+		
+		/*vBrowser = "FF";
+		if (vBrowser.equals("IE")){
+			myD = new InternetExplorerDriver();
+		} else {
+			myD = new FirefoxDriver();				
+		}
+		*/
+		
+		
+		
+		vURL = "http://www.google.co.uk";
+		vText = "Selenium Webdriver";
+		//exPath = "//*[@id="gbqfq"]"
+		//myD.get("http://www.chasestudentloans.com/");
+		myD.navigate().to(vURL);
+		
+		
+		//SENDKEYS
+		eXPath = "//*[@id=\"gbqfq\"]";
+		myD.findElement(By.xpath(eXPath)).sendKeys(vText);
+		Thread.sleep(lTime);
+		
+		//CLICK search
+		eXPath = "//*[@id='gbqfb']";
+		myD.findElement(By.xpath(eXPath)).click();
+		Thread.sleep(lTime);
+		
+		
+		//GET_TEXT results
+		eXPath = "//*[@id=\"resultStats\"]";
+		vgetText = myD.findElement(By.xpath(eXPath)).getText();
+		System.out.println("the first text searched is :" +vgetText);
+		Thread.sleep(lTime);
+		
+		//GET_TEXT results"Search"
+/*		eXPath = "//html/body/div[6]/div[2]/div/div[7]/div/div[4]/div/div[2]/div/ol/li/div/h3/a";
+		vgetText = myD.findElement(By.xpath(eXPath)).getText();
+		System.out.println("the second text is :" +vgetText);
+		Thread.sleep(lTime);*/
+		
+		//GET_ATTRIBUTES
+		eXPath = "//*[@id=\"gbqfq\"]";
+		vgetAttribute = myD.findElement(By.xpath(eXPath)).getAttribute(vAttribute);
+		System.out.println("the attribute is :" + vgetAttribute);
+		Thread.sleep(lTime);
+				
+		//close browser
+		myD.close();
+
+	}
+
+}
+
+/*
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,7 +94,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Hello world!
  *
  */
-public class App 
+/*public class App 
 {
 	@Test
     public void myTest()
@@ -24,12 +107,16 @@ public class App
        //WebDriver myD = new ChromeDriver();
         myD.navigate().to("https://www.google.co.uk/");
       
-        myD.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("Administrator");
-        myD.findElement(By.xpath("/html/body/form/div/table/tbody/tr[3]/td[2]/input")).sendKeys("1ncharge");
+        myD.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("Selenium WebDriver");
+        myD.findElement(By.xpath("//*[@id=\"lst-ib\"]")).click();
+        
+        
        // Thread.sleep(300);
-        myD.findElement(By.xpath("/html/body/form/div/table/tbody/tr[4]/td/input")).click();
+        
         
         
     }
 }
+
+*/
 
